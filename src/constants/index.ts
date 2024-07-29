21f383373b22966eed6f1717bd0b3cd52ebdae64
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2024-07-29 15:41:11 +0700

    fix setup

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+// label, type, name, value, placeholder
+
+export const LoginFormFields = [
+    ['Username', 'text', 'username', '', 'Enter your username'],
+    ['Password', 'password', 'password', '', 'Enter your password']
+]
+
+export const SignupFormFields = [
+    ['Email', 'email', 'email', '', 'Enter your email'],
+    ['Username', 'text', 'username', '', 'Enter your username'],
+    ['Password', 'password', 'password', '', 'Enter your password']
+]
