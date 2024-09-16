ec16c6e9735475c82b978fdae0cdaf0be4af6eb2
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2024-09-16 13:51:47 +0700

    add auth pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,40 @@
-export default function Login({}) {
+"use client";
+import FormGroup from "@/components/form/FormGroup";
+import { MemberProps } from "@/types";
+import { useMember } from "@/context/MemberContext";
+import { useRouter } from "next/navigation";
+
+function isMemberProps(obj: any): obj is MemberProps {
+  return obj && typeof obj.id == "number" && typeof obj.userName == "string";
+}
+
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { redirect?: string };
+}) {
+  const router = useRouter();
+  const { member, setMember } = useMember();
+  const redirectPath = searchParams.redirect || "/";
+  console.log(redirectPath);
+
+  const handleLogin = (res: any) => {
+    console.log("Login attempt:", res);
+    if (res === 403) {
+      console.log(member);
+      alert("Failed to login!");
+      window.location.reload();
+    }
+    if (isMemberProps(res) === true) {
+      setMember(res);
+      router.back();
+      // router.push(redirectPath);
+    }
+  };
+
   return (
-    <>Login</>
-  )
-};
+    <>
+      {member === null && <FormGroup formType="login" onSubmit={handleLogin} />}
+    </>
+  );
+}
